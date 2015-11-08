0a75bcd368b517e4f8a98fd055f2114902f5274c
Andrew Groff <andyg@rescuescg.com>
2015-11-08 15:13:49 -0800

    Adding bash configuration script to streamline application set-up

---
+++ configure.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+SOURCE_SETTINGS="bootstrap/settings.sample.php"
+DESTINATION_SETTINGS="bootstrap/settings.test.php"
+
+echo "You are about to configure the favorite restaurants app. This will configure the application's"
+echo "database settings,install dependencies, create a database, seed the database, and serve the"
+echo "application using PHP's built in web server."
+echo ""
+echo "Please ensure you have composer installed, as well as MySQL and PHP v5.5 or greater."
+echo "Continue?"
+
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "Proceeding."; break;;
+        No ) exit;;
+    esac
+done
+
+read -e -p "Please enter the mysql username: " USERNAME
+
+read -s -p "Please enter the mysql password: " PASSWORD
+
+echo ""
+
+echo $PASSWORD
+
+read -e -p "Please enter the mysql host (leave empty for 'localhost'): " HOST
+
+if [ -z "$HOST" ];
+then
+  HOST="localhost"
+fi
+
+echo ""
+echo "We will create a database if the user has permission. Otherwise use an existing one."
+read -e -p "Please enter the mysql database name (leave empty for 'restaurants'): " DATABASE
+
+
+if [ -z "$DATABASE" ];
+then
+  DATABASE="restaurants"
+fi
+
+cp $SOURCE_SETTINGS "$DESTINATION_SETTINGS.tmp"
+
+sed "s/{{host}}/$HOST/g" < "$DESTINATION_SETTINGS.tmp" > $DESTINATION_SETTINGS
+mv $DESTINATION_SETTINGS "$DESTINATION_SETTINGS.tmp"
+
+sed "s/{{database}}/$DATABASE/g" < "$DESTINATION_SETTINGS.tmp" > $DESTINATION_SETTINGS
+mv $DESTINATION_SETTINGS "$DESTINATION_SETTINGS.tmp"
+
+sed "s/{{username}}/$USERNAME/g" < "$DESTINATION_SETTINGS.tmp" > $DESTINATION_SETTINGS
+mv $DESTINATION_SETTINGS "$DESTINATION_SETTINGS.tmp"
+
+sed "s/{{password}}/$PASSWORD/g" < "$DESTINATION_SETTINGS.tmp" > $DESTINATION_SETTINGS
+rm "$DESTINATION_SETTINGS.tmp"
+
+composer install
+
+php install
+
+cd public
+
+echo "The application is running at http://localhost:8788"
+
+php -S localhost:8788
+
